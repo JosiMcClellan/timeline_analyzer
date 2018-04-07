2a20a86ccd07dd6e491dbfb766ca2b3a4588493a
Josi <junipertmcclellan@gmail.com>
2018-04-06 22:26:41 -0600

    design basic splash screen with brand images

--- src/App.js
+++ src/App.js
@@ -7,9 +7,12 @@ import './App.css';
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
+      <div className="app">
+        <header>
           <h1 className="App-title">Timeline Analyzer</h1>
+          <button>
+            Login with Github
+          </button>
         </header>
         <main>
           <BrowserRouter>
--- src/Splash.js
+++ src/Splash.js
@@ -1,3 +1,22 @@
-const Splash = () => 'Splash!';
+import React from 'react';
+
+const ServiceBrand = ({ name }) => (
+  <img
+    className="service-brand"
+    alt={name}
+    src={`/service_brands/${name.replace(' ', '-')}.png`}
+  />
+);
+
+const Splash = () => (
+  <div>
+    <h2>Welcome to Timeline Analyzer!</h2>
+    <p>We integrate with...</p>
+    <ServiceBrand name="octocat" />
+    <ServiceBrand name="pivotal tracker" />
+    <ServiceBrand name="travis" />
+    <ServiceBrand name="heroku" />
+  </div>
+);
 
 export default Splash;
